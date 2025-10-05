8548b624d225c6c86475c0b44c589f8ebde148ab
Rezaur Rahman <rezaur.official@gmail.com>
2025-10-05 22:21:56 +0600

    Added navbar, footer

---
+++ src/main/java/com/devrezaur/main/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.devrezaur.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping({"/", "/home"})
+    public String homePage() {
+        return "home-page";
+    }
+}
