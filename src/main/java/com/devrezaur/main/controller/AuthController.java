74a73508eb2e3e3d3319b9e1cbeebea249054f7e
Rezaur Rahman <rezaur.rahman@enosisbd.com>
2025-10-12 18:57:04 +0600

    Registration page added

--- src/main/java/com/devrezaur/main/controller/AuthController.java
+++ src/main/java/com/devrezaur/main/controller/AuthController.java
@@ -1,13 +1,21 @@
 package com.devrezaur.main.controller;
 
+import com.devrezaur.main.service.UserService;
 import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@RequiredArgsConstructor
 @Controller
 public class AuthController {
+
+  private final UserService userService;
 
   @GetMapping("/login")
   public String loginPage(@RequestParam @Nullable String logout, @RequestParam @Nullable String error, Model model) {
@@ -18,6 +26,32 @@ public class AuthController {
     }
 
     return "login-page";
+  }
+
+  @GetMapping("/register")
+  public String registrationPage() {
+    return "registration-page";
+  }
+
+  @PostMapping("/register")
+  public String doRegisterUser(@RequestParam String username, @RequestParam String password, RedirectAttributes redirectAttributes) {
+    try {
+      userService.registerUser(username, password);
+      redirectAttributes.addFlashAttribute("message", "Registration Successful!");
+    } catch (Exception ex) {
+      redirectAttributes.addFlashAttribute("error", ex.getMessage());
+    }
+
+    return "redirect:/register";
+  }
+
+  @GetMapping("/dashboard")
+  public String dashboardPage(HttpServletRequest httpServletRequest) {
+    if (httpServletRequest.isUserInRole("USER")) {
+      return "redirect:/user-dashboard";
+    }
+
+    return "redirect:/";
   }
 
 }
